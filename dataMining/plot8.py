4f54e62088e00c5cd2920a963c610aaf049c0e8f
Marvyn O'Rourke <marvyn81@hotmail.fr>
2017-02-23 15:31:04 +0100

    CSV nuée de points

--- dataMining/plot8.py
+++ dataMining/plot8.py
@@ -9,9 +9,9 @@ fig = plt.figure(figsize=(5,5))
 ax = fig.add_subplot(111)
 x=[]
 y=[]
-requete = c.execute("select TEMPS.fk_nom_fichier,t,sum(s) from (select fk_nom_fichier,sum(temps_tentative) as t from stats where fk_id_activite >20 group by fk_nom_fichier) as temps " +
-                    ",(select fk_nom_fichier,fk_id_activite,max(tentative),score as s from stats where fk_id_activite >20 group by fk_nom_fichier,fk_id_activite) as score " +
-                    "where TEMPS.fk_nom_fichier = SCORE.fk_nom_fichier and TEMPS.fk_nom_fichier in( select distinct (fk_nom_fichier)from stats where fk_id_activite=40) group by TEMPS.fk_nom_fichier")
+requete = c.execute("select TEMPS.fk_nom_fichier,t,sum(s) from (select fk_nom_fichier,sum(temps_tentative) as t from stats where fk_id_activite <=20 group by fk_nom_fichier) as temps " +
+                    ",(select fk_nom_fichier,fk_id_activite,max(tentative),score as s from stats where fk_id_activite <=20 group by fk_nom_fichier,fk_id_activite) as score " +
+                    "where TEMPS.fk_nom_fichier = SCORE.fk_nom_fichier and TEMPS.fk_nom_fichier in( select distinct (fk_nom_fichier)from stats where fk_id_activite=20) group by TEMPS.fk_nom_fichier")
 for row in requete:
     #390000 pour seq2 et 470000 pour seq1
     y.append(row[2]/390000 *100)
@@ -24,7 +24,7 @@ print(x)
 ###plt.show()
 ###fig.savefig("plot8a.png")
 ##
-##with open('dataseq2.csv','w') as csvfile:
-##    spamwriter = csv.writer(csvfile)
-##    spamwriter.writerow(x)
-##    spamwriter.writerow(y)
+with open('dataseq1.csv','w') as csvfile:
+    spamwriter = csv.writer(csvfile)
+    spamwriter.writerow(x)
+    spamwriter.writerow(y)
